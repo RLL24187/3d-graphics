cbd170337f3f76e7f63852fa860aab897532ad8b
rleongPd5 <rleong00@stuy.edu>
2020-03-14 18:52:55 -0400

    replaced 0.01 with step

--- parsefile.py
+++ parsefile.py
@@ -65,10 +65,10 @@ def parse_file( fname, edges, transform, screen, color ):
             add_box(edges, float(args[0]), float(args[1]), float(args[2]), float(args[3]), float(args[4]), float(args[5]))
 
         elif line == 'sphere':
-            add_sphere(edges, float(args[0]), float(args[1]), float(args[2]), float(args[3]), 0.01)
+            add_sphere(edges, float(args[0]), float(args[1]), float(args[2]), float(args[3]), step)
 
         elif line == 'torus':
-            add_torus(edges, float(args[0]), float(args[1]), float(args[2]), float(args[3]), float(args[4]), 0.01)
+            add_torus(edges, float(args[0]), float(args[1]), float(args[2]), float(args[3]), float(args[4]), step)
 
         elif line == 'circle':
             #print 'CIRCLE\t' + str(args)
